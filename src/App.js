b40ee34c1407806f00b6d51200aaafa9b74ee8a1
Ismael Elias <51543534+ismaeElias@users.noreply.github.com>
2020-10-19 18:13:07 -0300

    projeto finalizado

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import "./assets/index.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas :[]
+    };
+  }
+  criarNota(titulo, texto) {
+    const novaNota = {titulo,texto};
+    const novaArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novaArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
+}
+
+export default App;
